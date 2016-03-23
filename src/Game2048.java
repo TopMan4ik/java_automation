cfc1cf5496e55d254a93ccf3fccd1259a83b320a
gefgef <geforcer@i.ua>
2016-03-23 22:15:28 +0200

    update to game 2048

--- src/Game2048.java
+++ src/Game2048.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -33,6 +36,10 @@ import java.util.concurrent.TimeUnit;
 
 public class Game2048 extends TestHarness {
 
+    /**
+     * This is the constructor to create class instance with 2 variable we need
+     * @param chosenDriver String
+     */
     public Game2048(String chosenDriver) {
         this.chosenDriver = chosenDriver;
         this.url = "http://gabrielecirulli.github.io/2048/";
@@ -72,53 +79,68 @@ public class Game2048 extends TestHarness {
 
     public void playTheGame() {
         for (int stepsCounter = 0; stepsCounter <11; stepsCounter++) {
-            doRandomAction(getRandomInt(1,4));
-            printFieldState(stepsCounter);
+            //doRandomActionUsingWebDriver(getRandomInt(1,4));
+            doRandomActionUsingRobot(getRandomInt(1,4));
+            String[][] gameField = getFieldState();
+            printFieldState(gameField, stepsCounter);
             sleep (300);
         }
     }
 
-    public void printScore(){
-        System.out.println("Final score is: " + driver.findElement(scoreContainer).getText());
-    }
-
-    public void doRandomAction(int choice) {
+    public void doRandomActionUsingWebDriver(int choice) {
         Actions builder = new Actions(driver);
         switch (choice) {
-            case 1: builder.sendKeys(Keys.DOWN).build().perform();
-                break;
-            case 2: builder.sendKeys(Keys.UP).build().perform();
-                break;
-            case 3: builder.sendKeys(Keys.LEFT).build().perform();
-                break;
-            case 4: builder.sendKeys(Keys.RIGHT).build().perform();
-                break;
+            case 1: builder.sendKeys(Keys.DOWN).build().perform(); break;
+            case 2: builder.sendKeys(Keys.UP).build().perform(); break;
+            case 3: builder.sendKeys(Keys.LEFT).build().perform(); break;
+            case 4: builder.sendKeys(Keys.RIGHT).build().perform(); break;
         }
     }
 
-    public void printFieldState(int stepNumber) {
-        System.out.println("Step " + stepNumber + ":");
+    public void doRandomActionUsingRobot(int choice) {
+        try {
+            Robot robot = new Robot();
+            switch (choice) {
+                case 1: robot.keyPress(KeyEvent.VK_RIGHT); break;
+                case 2: robot.keyPress(KeyEvent.VK_LEFT); break;
+                case 3: robot.keyPress(KeyEvent.VK_UP); break;
+                case 4: robot.keyPress(KeyEvent.VK_DOWN); break;
+            }
+        } catch (Exception ex) {
+            System.out.println("Fuck! Something goes wrong!");
+        }
+    }
+
+    public String[][] getFieldState() {
         List<WebElement> tiles = driver.findElements(By.xpath("//div[@class='tile-container']/div"));
         String[][] gameField = new String[4][4];
         for (WebElement tile : tiles) {
             int lastIndex = tile.getAttribute("class").indexOf("tile-position-");
             String x = tile.getAttribute("class").substring(lastIndex+14, lastIndex+15);
-            int coordinateX = Integer.valueOf(x)-1;
             String y = tile.getAttribute("class").substring(lastIndex+16, lastIndex+17);
+            int coordinateX = Integer.valueOf(x)-1;
             int coordinateY = Integer.valueOf(y)-1;
             gameField[coordinateY][coordinateX] = tile.getText();
         }
-        for (int i=0; i<4; i++) {
-            for (int j=0; j<4; j++) {
-                if (gameField[i][j] == null)
+        return gameField;
+    }
+
+    public void printFieldState(String[][] gameField, int stepNumber) {
+        System.out.println("Step " + stepNumber + ":");
+        for (String[] row : gameField) {
+            for (String tile : row){
+                if (tile == null)
                     System.out.print("| 0 ");
-                else {
-                    System.out.print("| " + gameField[i][j] + " ");
-                }
+                else
+                    System.out.print("| " + tile + " ");
             }
             System.out.println("|");
         }
         System.out.println(" ");
+    }
+
+    public void printScore(){
+        System.out.println("Final score is: " + driver.findElement(scoreContainer).getText());
     }
 
 }
