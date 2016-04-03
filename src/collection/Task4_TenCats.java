1d1ffcef4851cebe277743a9c94f471bddf15ca9
gefgef <geforcer@i.ua>
2016-04-03 19:39:03 +0300

    task4

--- src/collection/Task4_TenCats.java
+++ src/collection/Task4_TenCats.java
@@ -1,7 +1,6 @@
 package collection;
 
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by gef on 03-Apr-16.
@@ -23,22 +22,37 @@ public class Task4_TenCats {
     }
 
     public static Map<String, Cat> createMap() {
-        //Ваш код
+        Map<String ,Cat> map = new LinkedHashMap<>();
+        for (int i=0; i<10; i++) {
+            Cat cat = new Cat("Kot-" + i);
+            map.put("Cat" + i, cat);
+        }
+        return map;
     }
 
     public static Set<Cat> convertMapToSet(Map<String, Cat> map) {
-        //Ваш код
+        Set<Cat> set = new HashSet<>();
+        for (Map.Entry<String, Cat> pair : map.entrySet()) {
+            Cat value = pair.getValue();
+            set.add(value);
+        }
+        return set;
     }
 
     public static void printCatSet(Set<Cat> set) {
-        //Ваш код
+        System.out.println("Cats: ");
+        for (Cat cat : set)
+            System.out.println(cat);
     }
 
     public static class Cat {
+
         private String name;
+
         public Cat(String name) {
             this.name = name;
         }
+
         public String toString() {
             return "Cat "+this.name;
         }
